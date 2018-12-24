92b5ffd35d4547070582dbe06e248d073b955d87
Abhishek Das <abhishekdas.0697@gmail.com>
2018-12-24 20:42:16 +0530

    Add files via upload

---
+++ Mathematics/Maximum Draws/Maximum Draws.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int T;
+    scanf("%d",&T);
+    for(int i=0;i<T;i++)
+    {
+        int n;
+        scanf("%d",&n);
+        printf("%d\n",n+1);
+    }
+    return 0;
+}
