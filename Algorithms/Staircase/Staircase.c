05a1793273b6dd51f15f722241e42a392b53d30e
Abhishek Das <abhishekdas.0697@gmail.com>
2019-06-22 16:16:43 +0530

    Add files via upload

---
+++ Algorithms/Staircase/Staircase.c
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    int n,i,j;
+    cin >> n ;
+    for(i=1;i<=n;i++)
+    {
+        for(j=n-i;j>0;j--)
+        {
+            cout<<(" ");
+        }
+
+        for(j=1;j<=i;j++)
+        {
+         cout<<("#");
+        }
+        cout<<endl;
+
+    }
+    return 0;
+}
